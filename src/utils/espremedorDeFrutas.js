53d03e063dc79abbaebd08b71432c45f15537329
ThalesAvila <dev.thales.avila@gmail.com>
2024-03-15 15:19:12 +0000

    function espremedorDeFrutas

---
+++ src/utils/espremedorDeFrutas.js
@@ -0,0 +1,11 @@
+export default function espremedorDeFrutas(fruta) {
+  if (fruta === 'laranja') {
+    return 'Sumo de laranja';
+  }
+  if (fruta === 'limão') {
+    return 'Sumo de limão';
+  }
+  if (fruta === 'abacaxi') {
+    return 'Sumo de abacaxi';
+  }
+}
