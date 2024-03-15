99dc828d5b7bd800778099d437b00837058b1c37
ThalesAvila <dev.thales.avila@gmail.com>
2024-03-15 16:22:10 +0000

    test: Increment Button

--- src/Components/Counter/Counter.test.jsx
+++ src/Components/Counter/Counter.test.jsx
@@ -4,9 +4,19 @@ import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+  screen.debug();
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {}
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  // Setup
+  const user = userEvent.setup();
+  render(<Counter />);
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  const counter = screen.getByTestId('current-count');
+  // Act
+  await user.click(incrementButton);
+  // Assert
+  expect(counter).toHaveTextContent('1');
+});
